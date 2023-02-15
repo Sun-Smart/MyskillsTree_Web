441ffca398b8fcba99cae6397754f555d2ae2261
MuthusamySunsmart <113328528+MuthusamySunsmart@users.noreply.github.com>
2023-02-15 14:51:10 +0530

    add skills

--- projects/n-tire-biz-app/src/app/pages/forms/mst-start-pages/mst-start-pages.component.ts
+++ projects/n-tire-biz-app/src/app/pages/forms/mst-start-pages/mst-start-pages.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
+import { ElementRef, Component, OnInit, Inject, Optional, ViewChild, EventEmitter } from '@angular/core';
 import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicDialog';
 import { mstapplicantmasterService } from '../../../service/mstapplicantmaster.service';
 import { SharedService } from '../../../../../../n-tire-biz-app/src/app/service/shared.service';
@@ -10,7 +10,10 @@ import { SessionService } from '../../../../../../n-tire-biz-app/src/app/pages/c
 import { ToastService } from '../../../../../../n-tire-biz-app/src/app/pages/core/services/toast.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { AppConstants, DropDownValues } from '../../../../../../n-tire-biz-app/src/app/shared/helper';
 import { mstapplicantmaster } from './../../../model/mstapplicantmaster.model';
+import { NgbDatepickerConfig, } from '@ng-bootstrap/ng-bootstrap';
+import { AttachmentComponent } from '../../../../../../n-tire-biz-app/src/app/custom/attachment/attachment.component';
 
 
 
@@ -24,14 +27,31 @@ export class MstStartPagesComponent implements OnInit {
   mstapplicantmaster_Form: FormGroup;
 
   formData: mstapplicantmaster;
+  bmyrecord: boolean = false;
   maxDate = undefined;
   data: any;
   p_menuid: any;
   p_currenturl: any;
   loginUser: any;
+  sessionData: any;
+  sourceKey: any;
+  SESSIONUSERID: any;//current user
+
+  applicanttype_List: DropDownValues[];
+  gender_List: DropDownValues[];
+  country_List: DropDownValues[];
+  state_List: DropDownValues[];
+  city_List: DropDownValues[];
+
+  country_optionsEvent: EventEmitter<any> = new EventEmitter<any>();//autocomplete
+  state_optionsEvent: EventEmitter<any> = new EventEmitter<any>();//autocomplete
+  city_optionsEvent: EventEmitter<any> = new EventEmitter<any>();//autocomplete
+  @ViewChild('profilephoto', { static: false }) profilephoto: AttachmentComponent;
+
 
   constructor(private route: Router, private fb: FormBuilder,
     private ngbDateParserFormatter: NgbDateParserFormatter,
+    private config: NgbDatepickerConfig,
     public dialogRef: DynamicDialogRef,
     public dynamicconfig: DynamicDialogConfig,
     public dialog: DialogService,
@@ -56,11 +76,12 @@ export class MstStartPagesComponent implements OnInit {
     this.p_currenturl = sharedService.currenturl;
 
     this.loginUser = localStorage.getItem('username');
+    let app_id = localStorage.getItem('applicantid');
     
     this.mstapplicantmaster_Form = this.fb.group({
       pk: [null],
       ImageName: [null],
-      applicantid: [null],
+      applicantid: app_id,
       firstname: [null, Validators.compose([Validators.required, Validators.pattern(/^[a-zA-Z ]*$/)])],
       lastname: [null, Validators.compose([Validators.required, Validators.pattern(/^[a-zA-Z ]*$/)])],
       emailid: [null, Validators.compose([Validators.required])],
@@ -97,9 +118,73 @@ export class MstStartPagesComponent implements OnInit {
 
   ngOnInit() {
 
+    this.sessionData = this.sessionService.getSession();
+    if (this.sessionData != null) {
+      this.SESSIONUSERID = this.sessionData.userid;
+    }
+
+    // getdefault Data
+    this.mstapplicantmaster_service.getDefaultData().then(res => {
+      this.applicanttype_List = res.list_applicanttype.value;
+      this.gender_List = res.list_gender.value;
+      this.country_List = res.list_country.value;
+    }).catch((err) => { this.spinner.hide(); console.log(err); });
   };
 
+// Type
+  applicanttype_onChange(evt: any) {
+    let e = this.f.applicanttype.value as any;
+    this.mstapplicantmaster_Form.patchValue({ applicanttypedesc: evt.options[evt.options.selectedIndex].text });
+  };
+
+  // Gender
+
+  gender_onChange(evt: any) {
+    let e = this.f.gender.value as any;
+    this.mstapplicantmaster_Form.patchValue({ genderdesc: evt.options[evt.options.selectedIndex].text });
+  }
+
+  onSelected_country(countryDetail: any) {
+    if (countryDetail.value && countryDetail) {
+      this.mstapplicantmaster_Form.patchValue({
+        country: countryDetail.value,
+        countrydesc: countryDetail.label,
+
+      });
+      this.mstapplicantmaster_service.getList_state(countryDetail.value).then(res => {
+        this.state_List = res as DropDownValues[]
+      }).catch((err) => { this.spinner.hide(); console.log(err); });
+
+    }
+  };
+
+  onSelected_state(stateDetail: any) {
+    if (stateDetail.value && stateDetail) {
+      this.mstapplicantmaster_Form.patchValue({
+        state: stateDetail.value,
+        statedesc: stateDetail.label,
+
+      });
+      this.mstapplicantmaster_service.getList_city(stateDetail.value).then(res => {
+        this.city_List = res as DropDownValues[]
+      }).catch((err) => { this.spinner.hide(); console.log(err); });
+
+    }
+  };
+
+  onSelected_city(cityDetail: any) {
+    if (cityDetail.value && cityDetail) {
+      this.mstapplicantmaster_Form.patchValue({
+        city: cityDetail.value,
+        citydesc: cityDetail.label,
+
+      });
+    }
+  };
+
+
   onSubmit() {
+    debugger;
     console.log("this.mstapplicantmaster_Form", this.mstapplicantmaster_Form);
 
     // if (!this.mstapplicantmaster_Form.valid) {
@@ -112,12 +197,13 @@ export class MstStartPagesComponent implements OnInit {
     console.log("this.formData", this.formData);
     this.spinner.show();
     this.mstapplicantmaster_service.saveOrUpdate_mstapplicantmastermains(this.formData).subscribe((res:any) => {
+      debugger;
       console.log("response",res);
-      
+      this.spinner.hide();
+      // this.route.navigate(['/home/newskilldetails']);
     });
   };
-
-  addSkills() {
+addskill(){
     this.route.navigate(['/home/newskilldetails']);
-  }
+}
 }
