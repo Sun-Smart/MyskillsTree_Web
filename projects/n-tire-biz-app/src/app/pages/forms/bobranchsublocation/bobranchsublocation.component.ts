36ba726e8ea11369734057fc71412998196af8e5
Kalaivanan <kalaivanan@sunsmart.co.in>
2023-02-22 11:59:31 +0530

    sublocation
    sublocation

--- projects/n-tire-biz-app/src/app/pages/forms/bobranchsublocation/bobranchsublocation.component.ts
+++ projects/n-tire-biz-app/src/app/pages/forms/bobranchsublocation/bobranchsublocation.component.ts
@@ -1,46 +1,19 @@
 import { bobranchsublocationService } from './../../../service/bobranchsublocation.service';
 import { bobranchsublocation } from './../../../model/bobranchsublocation.model';
-import { ElementRef, Component, OnInit, Inject, Optional, ViewChild, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { ToastService } from '../../../../../../n-tire-biz-app/src/app/pages/core/services/toast.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
-//Dropdown - nvarchar(5) - Backoffice -> Fixed Values menu
-
-//Custom error functions
-import { KeyValuePair, MustMatch, DateCompare, MustEnable, MustDisable, Time } from '../../../../../../n-tire-biz-app/src/app/shared/general.validator';
-
-//child table
-import { SmartTableDatepickerComponent, SmartTableDatepickerRenderComponent } from '../../../../../../n-tire-biz-app/src/app/custom/smart-table-datepicker.component';
-import { SmartTablepopupselectComponent, SmartTablepopupselectRenderComponent } from '../../../../../../n-tire-biz-app/src/app/custom/smart-table-popupselect.component';
-import { SmartTableFileRenderComponent } from '../../../../../../n-tire-biz-app/src/app/custom/smart-table-filerender.component';
-
-//Custom control
-import { durationComponent } from '../../../../../../n-tire-biz-app/src/app/custom/duration.component';
-import { LocalDataSource } from 'ng2-smart-table';
-import { Ng2SmartTableComponent } from 'ng2-smart-table';
-import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
-import { ShortcutInput, ShortcutEventOutput } from "ng-keyboard-shortcuts";
-//Shortcuts
+import { ShortcutInput } from "ng-keyboard-shortcuts";
 import { KeyboardShortcutsService } from "ng-keyboard-shortcuts";
-//translator
-import { TranslateService } from "@ngx-translate/core";
-//FK field services
-//detail table services
-import { switchMap, map, debounceTime } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { FormBuilder, FormGroup, FormControl, Validators, EmailValidator, ValidationErrors } from '@angular/forms';
-//primeng services
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { DynamicDialogRef } from 'primeng/dynamicDialog';
 import { DynamicDialogConfig } from 'primeng/dynamicDialog';
-import { FileUploadModule, FileUpload } from 'primeng/fileupload';
 import { DialogService } from 'primeng/dynamicDialog';
-//session,application constants
 import { SharedService } from '../../../../../../n-tire-biz-app/src/app/service/shared.service';
 import { SessionService } from '../../../../../../n-tire-biz-app/src/app/pages/core/services/session.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ThemeService } from '../../../../../../n-tire-biz-app/src/app/pages/core/services/theme.service';
-//custom fields & attachments
 import { AppConstants, DropDownValues } from '../../../../../../n-tire-biz-app/src/app/shared/helper';
 
 @Component({
@@ -49,24 +22,20 @@ import { AppConstants, DropDownValues } from '../../../../../../n-tire-biz-app/s
     styles: [],
     providers: [KeyboardShortcutsService]
 })
-
-
-
 export class bobranchsublocationComponent implements OnInit {
     formData: bobranchsublocation;
     list: bobranchsublocation[];
     bmyrecord: boolean = false;
     hidelist: any = [];
     objvalues: any = [];
-    viewHtml: any = '';//stores html view of the screen
-    showview: boolean = false;//view or edit mode
-    theme: string = "";//current theme
-    //formdata: any;//current form data
-    shortcuts: ShortcutInput[] = [];//keyboard keys
-    showSubmit: boolean = true;//button to show
+    viewHtml: any = '';
+    showview: boolean = false;
+    theme: string = "";
+    shortcuts: ShortcutInput[] = [];
+    showSubmit: boolean = true;
     showGoWorkFlow: boolean = false;
-    pkList: any;//stores values - used in search, prev, next
-    pkoptionsEvent: EventEmitter<any> = new EventEmitter<any>();//autocomplete of pk
+    pkList: any;
+    pkoptionsEvent: EventEmitter<any> = new EventEmitter<any>();
     toolbarVisible: boolean = true;
     customFieldServiceList: any;
     CustomFormName: string = "";
@@ -78,34 +47,19 @@ export class bobranchsublocationComponent implements OnInit {
     ShowTableslist: string[] = [];
     data: any;
     maindata: any;
-
     bfilterPopulate_bobranchsublocations: boolean = false;
     bobranchsublocation_menuactions: any = []
-
     bobranchsublocation_Form: FormGroup;
-
     locationid_List: DropDownValues[];
-
     private exportTime = { hour: 7, minute: 15, meriden: 'PM', format: 24 };
     showFormType: any;
     formid: any;
     pkcol: any;
-    SESSIONUSERID: any;//current user
-
+    SESSIONUSERID: any;
     sessionData: any;
     sourceKey: any;
-
-
-
-
-
-
-    constructor(
-        private nav: Location,
-        private translate: TranslateService,
-        private keyboard: KeyboardShortcutsService, private router: Router,
+    constructor( private router: Router,
         private themeService: ThemeService,
-        private ngbDateParserFormatter: NgbDateParserFormatter,
         public dialogRef: DynamicDialogRef,
         public dynamicconfig: DynamicDialogConfig,
         public dialog: DialogService,
@@ -114,29 +68,10 @@ export class bobranchsublocationComponent implements OnInit {
         private sharedService: SharedService,
         private sessionService: SessionService,
         private toastr: ToastService,
-        private sanitizer: DomSanitizer,
         private currentRoute: ActivatedRoute, private spinner: NgxSpinnerService) {
-        this.translate = this.sharedService.translate;
         this.data = dynamicconfig;
         this.p_menuid = sharedService.menuid;
         this.p_currenturl = sharedService.currenturl;
-        this.keyboard.add([
-            {
-                key: 'cmd l',
-                command: () => this.router.navigate(["/home/" + this.p_currenturl]),
-                preventDefault: true
-            },
-            {
-                key: 'cmd s',
-                command: () => this.onSubmitData(false),
-                preventDefault: true
-            },
-            {
-                key: 'cmd f',
-                command: () => this.resetForm(),
-                preventDefault: true
-            }
-        ]);
         this.bobranchsublocation_Form = this.fb.group({
             pk: [null],
             branchid: [null],
@@ -148,16 +83,10 @@ export class bobranchsublocationComponent implements OnInit {
             statusdesc: [null],
         });
     }
-
     get f() { return this.bobranchsublocation_Form.controls; }
-
-
-    //when child screens are clicked - it will be made invisible
     ToolBar(prop) {
         this.toolbarVisible = prop;
     }
-
-    //function called when we navigate to other page.defined in routing
     canDeactivate(): Observable<boolean> | boolean {
         debugger;
         if (this.bobranchsublocation_Form.dirty && this.bobranchsublocation_Form.touched) {
@@ -169,54 +98,20 @@ export class bobranchsublocationComponent implements OnInit {
         }
         return Observable.of(true);
     }
-
-    //check Unique fields
-
-    //navigation buttons
-    first() {
-        if (this.pkList.length > 0) this.PopulateScreen(this.pkList[0].pkcol);
-    }
-
-    last() {
-        if (this.pkList.length > 0) this.PopulateScreen(this.pkList[this.pkList.length - 1].pkcol);
-    }
-
-    prev() {
-        debugger;
-        let pos = this.pkList.map(function (e: any) { return e.sublocationid.toString(); }).indexOf(this.formid.toString());
-        if (pos > 0) this.PopulateScreen(this.pkList[pos - 1].pkcol);
-    }
-
-    next() {
-        debugger;
-        let pos = this.pkList.map(function (e: any) { return e.sublocationid.toString(); }).indexOf(this.formid.toString());
-        if (pos >= 0 && pos != this.pkList.length) this.PopulateScreen(this.pkList[pos + 1].pkcol);
-    }
-
-    //on searching in pk autocomplete
     onSelectedpk(pkDetail: any) {
         if (pkDetail.sublocationid && pkDetail) {
             this.PopulateScreen(pkDetail.pkcol);
         }
     }
-
-    // initialize
     async ngOnInit() {
-        //session & theme
         this.themeService.theme.subscribe((val: string) => {
             this.theme = val;
         });
-
         this.sessionData = this.sessionService.getSession();
         if (this.sessionData != null) {
             this.SESSIONUSERID = this.sessionData.userid;
         }
-
         this.theme = this.sessionService.getItem('selected-theme');
-        //this.viewHtml=this.sessionService.getViewHtml();
-
-        debugger;
-        //getting data - from list page, from other screen through dialog
         if (this.data != null && this.data.data != null) {
             this.data = this.data.data;
             this.maindata = this.data;
@@ -231,11 +126,9 @@ export class bobranchsublocationComponent implements OnInit {
         }
         let bobranchsublocationid = null;
 
-        //if view button(eye) is clicked
         if (this.currentRoute.snapshot.paramMap.get('viewid') != null) {
             this.pkcol = this.currentRoute.snapshot.paramMap.get('viewid');
             this.showview = true;
-            //this.viewHtml=this.sessionService.getViewHtml();
         }
         else if (this.currentRoute.snapshot.paramMap.get('usersource') != null) {
             this.pkcol = this.sessionService.getItem('usersource');
@@ -247,7 +140,6 @@ export class bobranchsublocationComponent implements OnInit {
             this.pkcol = this.currentRoute.snapshot.paramMap.get('id');
             this.showFormType = this.currentRoute.snapshot.paramMap.get('showFormType');
         }
-        //copy the data from previous dialog 
         this.viewHtml = ``;
         this.PopulateFromMainScreen(this.data, false);
         this.PopulateFromMainScreen(this.dynamicconfig.data, true);
@@ -255,20 +147,15 @@ export class bobranchsublocationComponent implements OnInit {
             this.ShowTableslist = this.currentRoute.snapshot.paramMap.get('tableid').split(',');
         }
         this.formid = bobranchsublocationid;
-        //alert(bobranchsublocationid);
-
-        //if pk is empty - go to resetting form.fill default values.otherwise, fetch records
         if (this.pkcol == null) {
             this.resetForm();
         }
         else {
             if (this.maindata == undefined || this.maindata == null || this.maindata.save == true) await this.PopulateScreen(this.pkcol);
-            //get the record from api
-            //foreign keys 
         }
         this.bobranchsublocation_service.getDefaultData().then(res => {
             this.locationid_List = res.list_locationid.value;
-        }).catch((err) => { this.spinner.hide(); console.log(err); });
+        }).catch((err) => { this.spinner.hide(); });
 
         //autocomplete
         this.bobranchsublocation_service.get_bobranchsublocations_List().then(res => {
@@ -276,13 +163,9 @@ export class bobranchsublocationComponent implements OnInit {
             this.pkoptionsEvent.emit(this.pkList);
         }
         ).catch((err) => { this.spinner.hide(); console.log(err); });
-        //setting the flag that the screen is not touched 
         this.bobranchsublocation_Form.markAsUntouched();
         this.bobranchsublocation_Form.markAsPristine();
     }
-
-
-
     resetForm() {
         if (this.bobranchsublocation_Form != null)
             this.bobranchsublocation_Form.reset();
@@ -378,20 +261,17 @@ export class bobranchsublocationComponent implements OnInit {
         return false;
     }
 
-
-
     async PopulateScreen(pkcol: any) {
         this.spinner.show();
         this.bobranchsublocation_service.get_bobranchsublocations_ByEID(pkcol).then(res => {
             this.spinner.hide();
-
             this.formData = res.bobranchsublocation;
             let formproperty = res.bobranchsublocation.formproperty;
             if (formproperty && formproperty.edit == false) this.showview = true;
             this.pkcol = res.bobranchsublocation.pkcol;
             this.formid = res.bobranchsublocation.sublocationid;
             this.FillData(res);
-        }).catch((err) => { console.log(err); });
+        }).catch((err) => { });
     }
 
     FillData(res: any) {
@@ -400,9 +280,6 @@ export class bobranchsublocationComponent implements OnInit {
         this.pkcol = res.bobranchsublocation.pkcol;
         this.bmyrecord = false;
         if ((res.bobranchsublocation as any).applicantid == this.sessionService.getItem('applicantid')) this.bmyrecord = true;
-        console.log(res);
-        //console.log(res.order);
-        //console.log(res.orderDetails);
         this.bobranchsublocation_Form.patchValue({
             branchid: res.bobranchsublocation.branchid,
             sublocationid: res.bobranchsublocation.sublocationid,
@@ -413,7 +290,6 @@ export class bobranchsublocationComponent implements OnInit {
             statusdesc: res.bobranchsublocation.statusdesc,
         });
         this.bobranchsublocation_menuactions = res.bobranchsublocation_menuactions;
-        //Child Tables if any
     }
 
     validate() {
@@ -445,7 +321,6 @@ export class bobranchsublocationComponent implements OnInit {
         ret = ret.replace(re, '');
         return ret;
     }
-
     async onSubmitDataDlg(bclear: any) {
         this.isSubmitted = true;
         if (!this.bobranchsublocation_Form.valid) {
@@ -453,15 +328,9 @@ export class bobranchsublocationComponent implements OnInit {
             return;
         }
         var obj = this.bobranchsublocation_Form.getRawValue();
-        console.log(obj);
         this.objvalues.push(obj);
         this.dialogRef.close(this.objvalues);
-        setTimeout(() => {
-            //this.dialogRef.destroy();
-        }, 200);
     }
-
-    //This has to come from bomenuactions & procedures
     afterAction(mode: any) {
         let formname = "";
         let query = "";
@@ -470,24 +339,10 @@ export class bobranchsublocationComponent implements OnInit {
         else if (mode == "refresh")
             this.router.navigate(['/home/' + formname + '/' + formname + '/edit/' + this.formid + query]);
     }
-
-
-
     async onSubmitData(bclear: any) {
-        debugger;
         this.isSubmitted = true;
         let strError = "";
-        // Object.keys(this.bobranchsublocation_Form.controls).forEach(key => {
-        //     const controlErrors: ValidationErrors = this.bobranchsublocation_Form.get(key).errors;
-        //     if (controlErrors != null) {
-        //         Object.keys(controlErrors).forEach(keyError => {
-        //             strError += '' + key + '' + keyError + '';
-        //         });
-        //     }
-        // });
         if (strError != "") return this.sharedService.alert(strError);
-
-
         if (!this.bobranchsublocation_Form.valid) {
             this.toastr.addSingle("error", "", "Enter the required fields");
             return;
@@ -505,12 +360,10 @@ export class bobranchsublocationComponent implements OnInit {
                 }
             }
         }
-        console.log(this.formData);
         this.spinner.show();
         this.bobranchsublocation_service.saveOrUpdate_bobranchsublocations(this.formData).subscribe(
             async res => {
                 this.spinner.hide();
-                debugger;
                 this.toastr.addSingle("success", "", "Successfully saved");
                 this.objvalues.push((res as any).bobranchsublocation);
                 if (!bclear) this.showview = true;
@@ -522,7 +375,6 @@ export class bobranchsublocationComponent implements OnInit {
                 else {
                     if (document.getElementById("contentAreascroll") != undefined) document.getElementById("contentAreascroll").scrollTop = 0;
                 }
-                this.clearList();
                 if (bclear) {
                     this.resetForm();
                 }
@@ -539,28 +391,16 @@ export class bobranchsublocationComponent implements OnInit {
                 this.bobranchsublocation_Form.markAsPristine();
             },
             err => {
-                debugger;
                 this.spinner.hide();
                 this.toastr.addSingle("error", "", err.error);
-                console.log(err);
             }
         )
     }
-
-
-
-
-    //dropdown edit from the screen itself -> One screen like Reportviewer
-    clearList() {
-    }
-
-
     PrevForm() {
         let formid = this.sessionService.getItem("key");
         let prevform = this.sessionService.getItem("prevform");
         this.router.navigate(["/home/" + prevform + "/" + prevform + "/edit/" + formid]);
     }
-
 }
 
 
