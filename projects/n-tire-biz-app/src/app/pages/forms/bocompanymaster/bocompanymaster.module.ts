c4d55ce39560536572105f13e7bd31e7ce3c307f
Kalaivanan <kalaivanan@sunsmart.co.in>
2023-02-22 12:33:21 +0530

    Company master code remove
    Company master code remove

--- projects/n-tire-biz-app/src/app/pages/forms/bocompanymaster/bocompanymaster.module.ts
+++ projects/n-tire-biz-app/src/app/pages/forms/bocompanymaster/bocompanymaster.module.ts
@@ -3,8 +3,6 @@ import { NgCommonModule } from '../../../../../../n-tire-biz-app/src/app/appcomm
 import { boreportviewerModule } from '../../../../../../n-tire-biz-app/src/app/pages/forms/boreportviewer/boreportviewer.module';
 import { routing } from './bocompanymaster.routing';
 import { bocompanymasterComponent } from './bocompanymaster.component';
-
-
 import { bocompanyholidayModule } from '../bocompanyholiday/bocompanyholiday.module';
 import { bofinancialyearModule } from '../bofinancialyear/bofinancialyear.module';
 import { bouserrolemasterModule } from '../bouserrolemaster/bouserrolemaster.module';
