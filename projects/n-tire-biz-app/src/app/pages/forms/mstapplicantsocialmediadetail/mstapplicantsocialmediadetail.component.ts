3e3c8ae775c3be7234cdcc4b0e16a538cf3af681
Kalaivanan <kalaivanan@sunsmart.co.in>
2023-02-27 16:43:59 +0530

    Corporate location,subcategory,social media details,skill details unwanted code remove
    Corporate location,subcategory,social media details,skill details unwanted code remove

--- projects/n-tire-biz-app/src/app/pages/forms/mstapplicantsocialmediadetail/mstapplicantsocialmediadetail.component.ts
+++ projects/n-tire-biz-app/src/app/pages/forms/mstapplicantsocialmediadetail/mstapplicantsocialmediadetail.component.ts
@@ -1,50 +1,23 @@
 import { mstapplicantsocialmediadetailService } from './../../../service/mstapplicantsocialmediadetail.service';
 import { mstapplicantsocialmediadetail } from './../../../model/mstapplicantsocialmediadetail.model';
-import { ElementRef, Component, OnInit, Inject, Optional, ViewChild, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, EventEmitter } from '@angular/core';
 import { ToastService } from '../../../../../../n-tire-biz-app/src/app/pages/core/services/toast.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
-//Dropdown - nvarchar(5) - Backoffice -> Fixed Values menu
-
-//Custom error functions
-import { KeyValuePair, MustMatch, DateCompare, MustEnable, MustDisable, Time } from '../../../../../../n-tire-biz-app/src/app/shared/general.validator';
-
-//child table
-import { SmartTableDatepickerComponent, SmartTableDatepickerRenderComponent } from '../../../../../../n-tire-biz-app/src/app/custom/smart-table-datepicker.component';
-import { SmartTablepopupselectComponent, SmartTablepopupselectRenderComponent } from '../../../../../../n-tire-biz-app/src/app/custom/smart-table-popupselect.component';
-import { SmartTableFileRenderComponent } from '../../../../../../n-tire-biz-app/src/app/custom/smart-table-filerender.component';
-
-//Custom control
-import { durationComponent } from '../../../../../../n-tire-biz-app/src/app/custom/duration.component';
-import { LocalDataSource } from 'ng2-smart-table';
-import { Ng2SmartTableComponent } from 'ng2-smart-table';
+import { DomSanitizer } from "@angular/platform-browser";
+import { KeyValuePair } from '../../../../../../n-tire-biz-app/src/app/shared/general.validator';
 import { NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
-import { ShortcutInput, ShortcutEventOutput } from "ng-keyboard-shortcuts";
-//Shortcuts
-import { KeyboardShortcutsService } from "ng-keyboard-shortcuts";
-//translator
-import { TranslateService } from "@ngx-translate/core";
-//FK field services
-//detail table services
-import { switchMap, map, debounceTime } from 'rxjs/operators';
+import { ShortcutInput } from "ng-keyboard-shortcuts";
 import { Observable } from 'rxjs';
-import { FormBuilder, FormGroup, FormControl, Validators, EmailValidator, ValidationErrors } from '@angular/forms';
-//primeng services
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DynamicDialogRef } from 'primeng/dynamicDialog';
 import { DynamicDialogConfig } from 'primeng/dynamicDialog';
-import { FileUploadModule, FileUpload } from 'primeng/fileupload';
 import { DialogService } from 'primeng/dynamicDialog';
-//session,application constants
 import { SharedService } from '../../../../../../n-tire-biz-app/src/app/service/shared.service';
 import { SessionService } from '../../../../../../n-tire-biz-app/src/app/pages/core/services/session.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ThemeService } from '../../../../../../n-tire-biz-app/src/app/pages/core/services/theme.service';
-//custom fields & attachments
 import { AppConstants, DropDownValues } from '../../../../../../n-tire-biz-app/src/app/shared/helper';
-import { Subject } from 'rxjs/Subject';
-import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
-import { createWorker, RecognizeResult } from 'tesseract.js';
 import { AttachmentComponent } from '../../../../../../n-tire-biz-app/src/app/custom/attachment/attachment.component';
 
 @Component({
@@ -67,8 +40,7 @@ import { AttachmentComponent } from '../../../../../../n-tire-biz-app/src/app/cu
           margin-top: 5px !important;
         }
       }
-    `],
-    providers: [KeyboardShortcutsService]
+    `]
 })
 
 
@@ -124,17 +96,8 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
     sessionData: any;
     sourceKey: any;
 
-
-
-
-
-
-    constructor(
-        private nav: Location,
-        private translate: TranslateService,
-        private keyboard: KeyboardShortcutsService, private router: Router,
+    constructor( private router: Router,
         private themeService: ThemeService,
-        private ngbDateParserFormatter: NgbDateParserFormatter,
         public dialogRef: DynamicDialogRef,
         public dynamicconfig: DynamicDialogConfig,
         public dialog: DialogService,
@@ -143,29 +106,10 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
         private sharedService: SharedService,
         private sessionService: SessionService,
         private toastr: ToastService,
-        private sanitizer: DomSanitizer,
         private currentRoute: ActivatedRoute, private spinner: NgxSpinnerService) {
-        this.translate = this.sharedService.translate;
         this.data = dynamicconfig;
         this.p_menuid = sharedService.menuid;
         this.p_currenturl = sharedService.currenturl;
-        this.keyboard.add([
-            {
-                key: 'cmd l',
-                command: () => this.router.navigate(["/home/" + this.p_currenturl]),
-                preventDefault: true
-            },
-            {
-                key: 'cmd s',
-                command: () => this.onSubmitData(false),
-                preventDefault: true
-            },
-            {
-                key: 'cmd f',
-                command: () => this.resetForm(),
-                preventDefault: true
-            }
-        ]);
         let app_id = localStorage.getItem('applicantid');
         this.mstapplicantsocialmediadetail_Form = this.fb.group({
             pk: [null],
@@ -194,7 +138,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
 
     //function called when we navigate to other page.defined in routing
     canDeactivate(): Observable<boolean> | boolean {
-        debugger;
         if (this.mstapplicantsocialmediadetail_Form.dirty && this.mstapplicantsocialmediadetail_Form.touched) {
             if (confirm('Do you want to exit the page?')) {
                 return Observable.of(true).delay(1000);
@@ -205,28 +148,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
         return Observable.of(true);
     }
 
-    //check Unique fields
-
-    //navigation buttons
-    first() {
-        if (this.pkList.length > 0) this.PopulateScreen(this.pkList[0].pkcol);
-    }
-
-    last() {
-        if (this.pkList.length > 0) this.PopulateScreen(this.pkList[this.pkList.length - 1].pkcol);
-    }
-
-    prev() {
-        debugger;
-        let pos = this.pkList.map(function (e: any) { return e.socialrefid.toString(); }).indexOf(this.formid.toString());
-        if (pos > 0) this.PopulateScreen(this.pkList[pos - 1].pkcol);
-    }
-
-    next() {
-        debugger;
-        let pos = this.pkList.map(function (e: any) { return e.socialrefid.toString(); }).indexOf(this.formid.toString());
-        if (pos >= 0 && pos != this.pkList.length) this.PopulateScreen(this.pkList[pos + 1].pkcol);
-    }
 
     //on searching in pk autocomplete
     onSelectedpk(pkDetail: any) {
@@ -255,9 +176,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
         }
 
         this.theme = this.sessionService.getItem('selected-theme');
-        //this.viewHtml=this.sessionService.getViewHtml();
-
-        debugger;
         //getting data - from list page, from other screen through dialog
         if (this.data != null && this.data.data != null) {
             this.data = this.data.data;
@@ -311,14 +229,14 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
         this.mstapplicantsocialmediadetail_service.getDefaultData().then(res => {
             this.applicantid_List = res.list_applicantid.value;
             this.socialmedianame_List = res.list_socialmedianame.value;
-        }).catch((err) => { this.spinner.hide(); console.log(err); });
+        }).catch((err) => { this.spinner.hide(); });
 
         //autocomplete
         this.mstapplicantsocialmediadetail_service.get_mstapplicantsocialmediadetails_List().then(res => {
             this.pkList = res as mstapplicantsocialmediadetail[];
             this.pkoptionsEvent.emit(this.pkList);
         }
-        ).catch((err) => { this.spinner.hide(); console.log(err); });
+        ).catch((err) => { this.spinner.hide(); });
         //setting the flag that the screen is not touched
         this.mstapplicantsocialmediadetail_Form.markAsUntouched();
         this.mstapplicantsocialmediadetail_Form.markAsPristine();
@@ -403,7 +321,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
             this.onSubmitData(false);
         }
         else if (this.maindata != null && (this.maindata.ScreenType == 1 || this.maindata.ScreenType == 2)) {
-            // this.onSubmitDataDlg(false);
             this.onSubmitData(false);
         }
         else {
@@ -464,7 +381,7 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
             this.pkcol = res.mstapplicantsocialmediadetail.pkcol;
             this.formid = res.mstapplicantsocialmediadetail.socialrefid;
             this.FillData(res);
-        }).catch((err) => { console.log(err); });
+        }).catch((err) => { });
     }
 
     FillData(res: any) {
@@ -473,9 +390,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
         this.pkcol = res.mstapplicantsocialmediadetail.pkcol;
         this.bmyrecord = false;
         if ((res.mstapplicantsocialmediadetail as any).applicantid == this.sessionService.getItem('applicantid')) this.bmyrecord = true;
-        console.log(res);
-        //console.log(res.order);
-        //console.log(res.orderDetails);
         this.mstapplicantsocialmediadetail_Form.patchValue({
             applicantid: res.mstapplicantsocialmediadetail.applicantid,
             applicantiddesc: res.mstapplicantsocialmediadetail.applicantiddesc,
@@ -484,7 +398,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
             socialmedianamedesc: res.mstapplicantsocialmediadetail.socialmedianamedesc,
             handlename: res.mstapplicantsocialmediadetail.handlename,
             url: res.mstapplicantsocialmediadetail.url,
-            // .replace(/<[^>]*>/g, '')
             remarks: res.mstapplicantsocialmediadetail.remarks,
             attachment: JSON.parse(res.mstapplicantsocialmediadetail.attachment),
             status: res.mstapplicantsocialmediadetail.status,
@@ -534,15 +447,11 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
         var obj = this.mstapplicantsocialmediadetail_Form.getRawValue();
         if (this.fileattachment.getAttachmentList() != null) obj.attachment = JSON.stringify(this.fileattachment.getAttachmentList());
         obj.fileAttachmentList = this.fileattachment.getAllFiles();
-        console.log(obj);
         await this.sharedService.upload(this.fileAttachmentList);
         this.attachmentlist = [];
         if (this.fileattachment) this.fileattachment.clear();
         this.objvalues.push(obj);
         this.dialogRef.close(this.objvalues);
-        setTimeout(() => {
-            //this.dialogRef.destroy();
-        }, 200);
     }
 
     //This has to come from bomenuactions & procedures
@@ -558,28 +467,14 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
 
 
     async onSubmitData(bclear: any) {
-        debugger;
         this.isSubmitted = true;
         let strError = "";
-
-
-
 
         if (!this.mstapplicantsocialmediadetail_Form.valid) {
             this.toastr.addSingle("error", "", "Enter the required fields");
             return;
         }
-        // Object.keys(this.mstapplicantsocialmediadetail_Form.controls).forEach(key => {
-        //     const controlErrors: ValidationErrors = this.mstapplicantsocialmediadetail_Form.get(key).errors;
-        //     if (controlErrors != null) {
-        //         Object.keys(controlErrors).forEach(keyError => {
-        //             strError += 'control: ' + key + ', Error: ' + keyError + '<BR>';
-        //         });
-        //     }
-        // });
         if (strError != "") return this.sharedService.alert(strError);
-
-
 
         if (!this.validate()) {
             return;
@@ -596,7 +491,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
         }
         if (this.fileattachment.getAttachmentList() != null) this.formData.attachment = JSON.stringify(this.fileattachment.getAttachmentList());
         this.fileAttachmentList = this.fileattachment.getAllFiles();
-        console.log(this.formData);
         this.spinner.show();
         this.mstapplicantsocialmediadetail_service.saveOrUpdate_mstapplicantsocialmediadetails(this.formData).subscribe(
             async res => {
@@ -604,7 +498,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
                 this.attachmentlist = [];
                 if (this.fileattachment) this.fileattachment.clear();
                 this.spinner.hide();
-                debugger;
                 this.toastr.addSingle("success", "", "Successfully saved");
                 this.sessionService.setItem("attachedsaved", "true")
                 this.objvalues.push((res as any).mstapplicantsocialmediadetail);
@@ -617,7 +510,6 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
                 else {
                     if (document.getElementById("contentAreascroll") != undefined) document.getElementById("contentAreascroll").scrollTop = 0;
                 }
-                this.clearList();
                 if (bclear) {
                     this.resetForm();
                 }
@@ -634,21 +526,11 @@ export class mstapplicantsocialmediadetailComponent implements OnInit {
                 this.mstapplicantsocialmediadetail_Form.markAsPristine();
             },
             err => {
-                debugger;
                 this.spinner.hide();
                 this.toastr.addSingle("error", "", err.error);
-                console.log(err);
             }
         )
     }
-
-
-
-
-    //dropdown edit from the screen itself -> One screen like Reportviewer
-    clearList() {
-    }
-
 
     PrevForm() {
         let formid = this.sessionService.getItem("key");
